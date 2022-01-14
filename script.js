f90a20e13ea70c6261b585897ef8cd1a517c2bbd
Srikhar Shashi D <dasojusrikhar@gmail.com>
2022-01-15 01:55:44 +0530

    good amt of js done

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 const addbutton=document.querySelector(".addbutton");
 const modalbg=document.querySelector(".modal-bg");
 const closebutton=document.querySelector(".closebutton");
+const bookname=document.querySelector("#booknameip");
+const authorname=document.querySelector("#authornameip");
+const submitbutton=document.querySelector(".submit")
+
 addbutton.addEventListener("click",()=>{
     modalbg.classList.add("bg-active");
 });
+
 closebutton.addEventListener("click",()=>
 {
     modalbg.classList.remove("bg-active");
@@ -22,3 +27,14 @@ function Book(name,author,status) {
     this.status=status;
 }
   
+submitbutton.addEventListener("click",()=>{
+    if(bookname.value!=="" && authorname.value!=="")
+    {
+        let newbook=new Book(bookname.value,authorname.value,false);
+        myLibrary.push(newbook);
+        bookname.value="";
+        authorname.value="";
+        console.log(myLibrary);
+        modalbg.classList.remove("bg-active");
+    }
+});
